7c1e53c7904c1a2fec3fbdd8c217605c14705f96
moritz-laber <moritz.laber@gmail.com>
2026-02-10 11:33:13 -0500

    Add tables for generalization bounds.

--- table_generalization_realworld.py
+++ table_generalization_realworld.py
@@ -4,62 +4,46 @@ table_generalization_realworld.py
 
 Creates a table with generalization results from
 experiments on realworld data.
+
+M. Laber, 2026/02
 """
 
 ### IMPORTS ###
-import matplotlib.pyplot as plt
-import pickle 
-import numpy as np
-import jax
-import jax.numpy as jnp
-import os
+import pickle
 import pandas as pd
+import numpy as np
 
-### Parameters ###
-savetable = False                      # whether to save the table
-input_dir = './experiments/realworld/' # directory with results
-output_dir = './plots/generalization/' # directory for saving table 
-datasets = [                           # list of datasets to consider
-    'cornell',
-    'wisconsin',
-    'texas',
-    'cora',
-    'citeseer',
-    'chameleon',
-    'squirrel'
-    ]
-layers = [2,3]        # number of layers
-reps =  [             # number of repetitions to try
-    125,
-    250,
-    500,
-    1000,
-    2000,
-    4000
-    ]
+### PARAMETERS ###
+savetable = True  # whether to save the table
+input_dir = './experiments/realworld/'  # directory with results
+output_dir = './tables/'                # directory to save the table
 
+### MAIN ###
+
+# load the data
 result_df = []
-for dataset in datasets:
-    for l in layers:
-        # load data
+for dataset in ['cornell', 'wisconsin', 'texas', 'cora', 'citeseer', 'chameleon', 'squirrel']:
+    for l in [2,3]:
         success = False
-        for r in reps:
+        for r in [125, 250, 500, 1000, 2000, 4000]:
             try:
-                with open(f'{input_dir}{dataset}/multiclass_generalization_model=sgc_data={dataset}_l={l}_reps={r}.pkl','rb') as f:
+                path_to_file = f'{input_dir}{dataset}/generalization_model=sgc_data={dataset}_l={l}_reps={r}.pkl'
+                    
+                with open(path_to_file,'rb') as f:
                     result = pickle.load(f)
                 success = True
                 break
+
             except:
                 pass
-        
-        # create row in table
+
         if success:
             result_df.append([dataset,
                               l,
                               np.max(result['empirical_LHS']),
-                              result['bound'],              # we used the wrong Cl in experiments
+                              result['bound'],                  
                               result['lipschitz_FCD'],
-                              result['loss_lipschitz_l2'],      # we used the wrong Cl in experiments
+                              result['loss_lipschitz_l2'],      
                               result['M'],
                               result['epsilon'],
                               result['delta'],
@@ -72,7 +56,7 @@ for dataset in datasets:
             row = [dataset, l] + 10 * [pd.NA]
             result_df.append(row)
 
-# creat table from rows
+# create the table
 result_df = pd.DataFrame(result_df, columns=['dataset',
                                              'num_layers',
                                              'empirical_LHS',
@@ -86,7 +70,6 @@ result_df = pd.DataFrame(result_df, columns=['dataset',
                                              'pearson',
                                              'K',
                                              'Chi'])
-# save table
+
 if savetable:
-    result_df.to_csv(f'{output_dir}/generalization_realworld.csv', index=False)
-    result_df.to_latex(f'{output_dir}/generalization_realworld.tex', index=False, float_format="%.2f")
+    result_df.to_csv(f'{output_dir}generalization_realworld.csv', index=False)
--- table_generalization_synthetic.py
+++ table_generalization_synthetic.py
@@ -4,29 +4,26 @@ table_generalization_synthetic.py
 
 Creates a table with generalization results from
 experiments on synthetic data.
+
+M. Laber, 2026/02
 """
 
 ### IMPORTS ###
-import matplotlib.pyplot as plt
-import pickle 
-import numpy as np
-import jax
-import jax.numpy as jnp
-import os
+import pickle
 import pandas as pd
+import numpy as np
 
 ### PARAMETERS ###
-savetable = False                        # whether to save the table
-input_dir = './experiments/synthetic/'   # directory with results
-output_dir = './plots/generalization/'   # directory for saving table 
-r = 1000                                 # number of training repetitions
-layers = [2, 3]                          # number of layers
-correlations = ['inif', 'indf', 'dndf']  # correlations
+savetable = True                        # whether to save the table
+input_dir = './experiments/synthetic/'  # directory with results
+output_dir = './tables/'                # directory to save the table
+r = 1000                                # number of training repetitions
 
+### MAIN ###
 result_df = []
-for corr in correlations:
-    for l in layers:
-        # load data
+for corr in ['inif', 'indf', 'dndf']:
+    for l in [2,3]:
+
         success = False
         try:
             with open(f'{input_dir}{corr}/generalization_model=sgc_corr={corr}_l={l}_reps={r}.pkl','rb') as f:
@@ -35,13 +32,13 @@ for corr in correlations:
         except:
             continue
 
-        # create a row in the table
         if success:
             result_df.append([corr,
                               l,
                               np.max(result['empirical_LHS']),
                               result['bound'],
                               result['lipschitz_FCD'],
+                              result['loss_lipschitz_l2'], 
                               result['M'],
                               result['epsilon'],
                               result['delta'],
@@ -54,12 +51,13 @@ for corr in correlations:
             row = [corr, l] + 10 * [pd.NA]
             result_df.append(row)
 
-# create the table from rows
+# create the table
 result_df = pd.DataFrame(result_df, columns=['corr',
                                              'num_layers',
                                              'empirical_LHS',
                                              'upper_bound',
                                              'lipschitz_FCD',
+                                             'lipschitz_loss',
                                              'M',
                                              'epsilon',
                                              'delta',
@@ -67,7 +65,6 @@ result_df = pd.DataFrame(result_df, columns=['corr',
                                              'pearson',
                                              'K',
                                              'Chi'])
-# save the table
+
 if savetable:
-    result_df.to_csv(f'{output_dir}/generalization_synthetic.csv', index=False)
-    result_df.to_latex(f'{output_dir}/generalization_synthetic.tex', index=False, float_format="%.2f")
+    result_df.to_csv(f'{output_dir}generalization_synthetic.csv', index=False)
